7be8969ad35b3d2e84367a6830b5dfea873ab14f
Zahidul Islam  Sagor <117823487+sagor2315@users.noreply.github.com>
2024-08-26 11:44:41 +0600

    another new file created inside the modules name product.route.ts with code

---
+++ src/app/modules/product.route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { productController } from "./product.controller";
+
+const router = express.Router();
+router.post("/create-product", productController.createProduct);
+router.get("/", productController.getAllProduct);
+router.get("/:productId", productController.getSinglProduct);
+router.put("/:productId", productController.updateSingleProduct);
+router.delete("/:productId", productController.deleteSingleproduct);
+
+export const productRouter = router;
